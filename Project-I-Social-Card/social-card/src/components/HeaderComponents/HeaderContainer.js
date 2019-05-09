ed079e8b96e427250b9ea53ccb24b8fa1279b3ec
Jeremy Morales <jeremy.morales@gmail.com>
2019-05-08 20:48:25 -0700

    everything running

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = props => {
+    return (
+        <header>
+            <ImageThumbnail />
+            {/* <HeaderTitle />
+            <HeaderContent /> */}
+        </header>
+    )
+}
+
+export default HeaderContainer;
