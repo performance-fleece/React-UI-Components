ffd8df2eff3e96d08f0076b80142d32988794c3e
Jeremy Morales <jeremy.morales@gmail.com>
2019-05-10 19:49:04 -0700

    mvp complete

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -6,12 +6,13 @@ import CardContent from './CardContent';
 const CardBanner = props => {
     return (
 
-        <a href="https://reactjs.org">
         <div className="banner-box">
+        <a href="https://reactjs.org">
+
         <img className="banner-img" src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"></img>
+        </a>
         <CardContent />
         </div>
-        </a>
         
     )
 }
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,13 +3,13 @@ import './Card.css';
 
 const CardContent = props => {
     return (
-        // <a href="https://reactjs.org">
+        <a href="https://reactjs.org">
          <div className='content-card'>
             <h4>Get Started with React</h4>
             <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
             <a href="https://reactjs.org">reactjs.org</a>
         </div>
-        // </a>
+        </a>
        
     )
 
