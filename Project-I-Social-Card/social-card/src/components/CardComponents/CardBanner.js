01af8a87f6dc85d11905f4e4edb0f86b5965b20c
Jeremy Morales <jeremy.morales@gmail.com>
2019-05-11 12:16:08 -0700

    resolved issues

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import './Card.css';
-<<<<<<< HEAD
-
-const CardBanner = props => {
-    return (
-        <div className="banner-box">
-        <img className="banner-img" src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"></img>
-        </div>
-    )
-};
-=======
 import CardContent from './CardContent';
 
 
@@ -26,6 +16,5 @@ const CardBanner = props => {
         
     )
 }
->>>>>>> ffd8df2eff3e96d08f0076b80142d32988794c3e
 
 export default CardBanner;
