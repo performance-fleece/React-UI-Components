f5ac4f747a62a859ae5748701146007d68f16203
Jeremy Morales <jeremy.morales@gmail.com>
2019-05-10 19:42:36 -0700

    making clickable

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './CardContent';
+
 
 const CardBanner = props => {
     return (
+
+        <a href="https://reactjs.org">
         <div className="banner-box">
         <img className="banner-img" src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"></img>
+        <CardContent />
         </div>
+        </a>
+        
     )
 }
 
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -3,11 +3,14 @@ import './Card.css';
 
 const CardContent = props => {
     return (
-        <div className='content-card'>
+        // <a href="https://reactjs.org">
+         <div className='content-card'>
             <h4>Get Started with React</h4>
             <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-            <a href="https://react.js.org">reactjs.org</a>
+            <a href="https://reactjs.org">reactjs.org</a>
         </div>
+        // </a>
+       
     )
 
 }
